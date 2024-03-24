6d9d7048822e8980aed77776e1593ca2f5693b5d
André Bellico <mhsandrevsbellico@gmail.com>
2024-03-24 11:44:06 -0300

    fix: switch language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,7 @@ import type { Metadata } from 'next'
 // eslint-disable-next-line camelcase
 import { Josefin_Sans } from 'next/font/google'
 import { Toaster } from '@/components/ui/toaster'
-import './globals.css'
 import { RenderContent } from '@/components/RenderContent'
-
-// import global_en from '../translations/en/global.json'
-// import global_br from '../translations/pt-br/global.json'
-// import i18next from 'i18next'
-
-// i18next.init({
-//   interpolation: { escapeValue: false },
-//   lng: 'en',
-//   resources: {
-//     en: {
-//       global: global_en,
-//     },
-//     'pt-br': {
-//       global: global_br,
-//     },
-//   },
-// })
 
 export const metadata: Metadata = {
   title: '| Portfólio',
@@ -44,10 +26,8 @@ export default function RootLayout({
         <title>Bellico Portfolio</title>
       </head>
       <body>
-        {/* <I18nextProvider i18n={i18next}> */}
         <RenderContent>{children}</RenderContent>
         <Toaster />
-        {/* </I18nextProvider> */}
       </body>
     </html>
   )
